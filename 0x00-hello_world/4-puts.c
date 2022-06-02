8e4e44eeb8ec0f2455303af741ae14108d132d88
GodblessMe2 <oshindeinde@gmail.com>
2022-06-02 15:21:05 +0000

    First C programming code on puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (It's Successful)
+ */
+int main(void)
+{
+puts("How to print text using printf, puts and putchar\n");
+return (0);
+}
