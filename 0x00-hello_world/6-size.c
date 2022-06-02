4c123fce6c1c51284385d4f77d100c04ddeefc97
GodblessMe2 <oshindeinde@gmail.com>
2022-06-02 17:30:21 +0000

    First C programming code on size on the computer

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ float floatType;
 char charType;
 long int longIntType;
 long long int longLongInt;
-printf("Size of a char: %d bytes\n", sizeof(charType));
-printf("Size of a int: %d bytes\n", sizeof(intType));
-printf("Size of a float: %d bytes\n", sizeof(floatType));
-printf("Size of a long int: %ld bytes\n", sizeof(longIntType));
-printf("Size of a long long int: %d bytes\n", sizeof(longLongInt));
+printf("Size of a char: %d byte(s)\n", sizeof(charType));
+printf("Size of a int: %d byte(s)\n", sizeof(intType));
+printf("Size of a float: %d byte(s)\n", sizeof(floatType));
+printf("Size of a long int: %ld byte(s)\n", sizeof(longIntType));
+printf("Size of a long long int: %d byte(s)\n", sizeof(longLongInt));
 return (0);
 }
