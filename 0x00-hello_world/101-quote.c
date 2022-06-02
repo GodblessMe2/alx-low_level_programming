405d01c3f521ba2ec765e33c1725303b910a5a31
GodblessMe2 <oshindeinde@gmail.com>
2022-06-02 22:17:40 +0000

    First C programming code on correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 /**
  * main - Entry point
+ * main c
+ * print a string
  *
- * Return : Always 0 (Success)
+ * Return : Always 1 (Success)
  */
-int main(void)
+void main(void)
 {
-char text[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
-write(2, text, sizeof(text));
+char *str;
+int i;
+str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+for (i = 0; i < 58; i++)
+{
+putchar(str[i]);
+}
+putchar('\n');
 return (1);
 }
