a0a99d620a39a5e69e041ab2dd4672d4d8281bf2
GodblessMe2 <oshindeinde@gmail.com>
2022-06-02 15:38:52 -0700

    correction

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,12 +10,15 @@
  * compile it in 32 bits with warning
  * Return: Always 0 (Success)
  */
+
 int main(void)
-{i
+{
 printf("Size of a char: %lu byte(s)\n", sizeof(char));
 printf("Size of an int: %lu byte(s)\n", sizeof(int));
 printf("Size of a long int: %lu byte(s)\n", sizeof(long));
 printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
 printf("Size of a float: %lu byte(s)\n", sizeof(float));
-return (0)
+
+return (0);
+
 }
