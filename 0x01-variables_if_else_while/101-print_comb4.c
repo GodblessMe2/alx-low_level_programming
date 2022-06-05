e1c9a81d35684c059c83da065c1b21bf8c1ca269
GodblessMe2 <oshindeinde@gmail.com>
2022-06-05 11:55:01 +0000

    correction fix

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,9 +10,9 @@ int main(void)
 int number1, number2, number3;
 for (number1 = 0; number1 < 8; number1++)
 {
-for (number2 = 1; number2 < 9; number2++)
+for (number2 = number1 + 1; number2 < 9; number2++)
 {
-for (number3 = 2; number3 < 10; number3++)
+for (number3 = number2 + 1; number3 < 10; number3++)
 {
 putchar((number1 % 10) + '0');
 putchar((number2 % 10) + '0');
--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -14,7 +14,6 @@ for (num2 = num1 + 1; num2 <= 99; num2++)
 {
 putchar((num1 / 10) + '0');
 putchar((num1 % 10) + '0');
-putchar(',');
 putchar(' ');
 putchar((num2 / 10) + '0');
 putchar((num2 % 10) + '0');
