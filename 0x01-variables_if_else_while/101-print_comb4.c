3b0c031f6367cf1b17373704be84c228031bc211
GodblessMe2 <oshindeinde@gmail.com>
2022-06-05 11:47:15 +0000

    correction fix

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,9 +8,9 @@
 int main(void)
 {
 int number1, number2, number3;
-for (number1 = 0; number1 < 10; number1++)
+for (number1 = 0; number1 < 8; number1++)
 {
-for (number2 = 1; number2 < 10; number2++)
+for (number2 = 1; number2 < 9; number2++)
 {
 for (number3 = 2; number3 < 10; number3++)
 {
