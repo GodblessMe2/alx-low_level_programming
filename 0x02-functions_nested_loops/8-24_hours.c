319b27aa6fd8192e8fcebcdb5f1acceafc25606c
GodblessMe2 <oshindeinde@gmail.com>
2022-06-07 22:30:47 +0000

    Last Digit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,13 +10,13 @@ void jack_bauer(void)
 int hours, minute;
 for (hours = 0; hours <= 23; hours++)
 {
-for (minute = 0; minute <= 59; mintue++)
+for (minute = 0; minute <= 59; minute++)
 {
 _putchar((hour / 10) + '0');
 _putchar((hour % 10) + '0');
 _putchar(';');
 _putchar((minute / 10) + '0');
-_putchar((minutue % 10) + '0');
+_putchar((minute % 10) + '0');
 -putchar('\n');
 }
 }
