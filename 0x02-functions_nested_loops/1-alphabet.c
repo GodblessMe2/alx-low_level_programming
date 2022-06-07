fa8ba594a96966aeca54ad4a6270387a9c91f0a1
GodblessMe2 <oshindeinde@gmail.com>
2022-06-07 10:00:00 -0700

    print alphabeth

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabeth - prints the alphabet, in lowercase.
+ */
+void print_alphabeth(void)
+{
+char alphabeth = 'a';
+
+while (alphabeth <= 'z');
+{
+alphabeth ++;
+_putchar(alphabeth);
+
+_putchar('\n');
+
+}
+}
+
