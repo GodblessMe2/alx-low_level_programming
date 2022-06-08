457633012d9138b291695947a2a856209c5c76a2
GodblessMe2 <oshindeinde@gmail.com>
2022-06-08 17:33:33 +0000

    Multiplication of 9

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table.
+ * @n: if greater than or less than.
+ */
+void print_times_table(int n)
+{
+int num, times, result;
+if (n > 15 || n < 0)
+{
+for (num = 0; num <= n, num++)
+{
+_putchar('0');
+
+for (times = 1; times = 2; times++)
+{
+_putchar(',');
+_putchar(' ');
+
+result = num * times;
+
+if (result <= 99)
+{
+_putchar(' ');
+}
+if (result <= 9)
+{
+_putchar(' ');
+}
+if (result >=100)
+{
+_putchar((result / 100) + '0');
+_putchar(((result / 10)) % 10 + '0'0
+}
+else if (result <= 99 && result >= 10)
+{
+_putchar((result / 10) + '0');
+}
+_putchar((result % 10) + '0');
+}
+_putchar('\n);
+}
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,5 +13,7 @@ void times_table(void);
 void print_alphabet(void);
 int add(int, int);
 void print_to_98(int n);
+void print_times_table(int n);
+
 
 #endif
