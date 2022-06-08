f76ba28f7a8eada3662983bbcc693e9dbe941f5e
GodblessMe2 <oshindeinde@gmail.com>
2022-06-08 09:50:43 +0000

    Multiplication of 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.	h"
+
+/**
+ * time_table - prints the 9 times table, starting with 0.
+ *
+ */
+
+void times_table(void)
+{
+int num = 0, result, times;
+
+while (num <= 9) 
+{
+num++;
+_putchar('0');
+
+for (times = 1; times <= 9; times++)
+{
+_putchar(',');
+_putchar(' ');
+
+result = num * times;
+
+if (result <= 9)
+_putchar(' ');
+else
+_putchar((result / 10) + '0');
+_putchar((result % 10) + '0');
+
+}
+_putchar('\n');
+}	
+}
