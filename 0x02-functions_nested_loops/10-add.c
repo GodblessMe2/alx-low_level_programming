4016fff3e9197fb331af1b8b1a130e2f0f922b03
GodblessMe2 <oshindeinde@gmail.com>
2022-06-08 12:35:30 +0000

    Multiplication of 9

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers and returns the result.
+ *
+ * Return: result
+ */
+
+int add(int sumA, int sumB)
+{
+
+return (sumA + sumB);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,6 +9,8 @@ int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
-void times_table(void);
+void times_table(void)i;
+void print_alphabet(void);
+int add(int, int);
 
 #endif
