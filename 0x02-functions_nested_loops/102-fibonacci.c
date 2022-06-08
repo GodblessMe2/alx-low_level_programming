5325aa746bed3c7584fa9f4efed45a922c5fb00d
GodblessMe2 <oshindeinde@gmail.com>
2022-06-08 18:22:45 +0000

    Multiplication of 9

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the first 50 Fibonacci numbers, starting with 1 and 2.
+ *
+ * Return: always 0.
+ */
+
+int main(void)
+{
+int i;
+unsigned long fib1 = 0, fib2 = 1, sum;
+
+for (i = 0; i < 50; count++)
+{
+sum = fib1 + fib2;
+fib2 = sum;
+
+if (i == 49)
+{
+printf("\n");
+}
+else
+{
+printf(", ");
+}
+}
+
+return (0);
+
+}
