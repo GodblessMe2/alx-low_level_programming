8414a1869c66c6e72c5db09aac4e032b9d69e451
GodblessMe2 <oshindeinde@gmail.com>
2022-06-08 18:07:58 +0000

    Multiplication of 9

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio>
+
+/**
+ * main -  prints the sum of all the multiples of 3 or 5 below 1024.
+ *
+ * Return: always 0.
+ */
+
+int main(void)
+{
+int num, sum = 0;
+
+for (num = 0; num < 1024; n++)
+{
+if ((num % 5) == 0 || (i % 3) == 0)
+{
+sum += num;
+}
+}
+printf("%d\n"), sum);
+
+return (0);
+
+}
