bbeee50e8e55cacd27783041c3a60420337b4cc1
GodblessMe2 <oshindeinde@gmail.com>
2022-06-09 14:39:12 +0000

    Debugging the 1-main.c

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,6 +6,6 @@
 void positive_or_negative(int i);
 int largest_number(int, int, int);
 int convert_day(int month, int day);
-void print_remainig_day(int month, int day, int year);
+void print_remainig_days(int month, int day, int year);
 
 #endif
