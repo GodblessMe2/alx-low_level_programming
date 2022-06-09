065d4eee80fb5c9864a75e537cc665053102ecc2
GodblessMe2 <oshindeinde@gmail.com>
2022-06-09 03:57:35 -0700

    Debugging

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,5 +3,11 @@
 
 int _putchar(char c);
 void print_alphabet(void);
+void print_alphabet(void);
+int _islower(int c);
+int _isalpha(int c);
+int _print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
 
 #endif
