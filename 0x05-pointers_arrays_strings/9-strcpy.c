a917f74e249a800e6b3a4dee8954b84ea3456596
GodblessMe2 <oshindeinde@gmail.com>
2022-06-13 23:28:28 +0000

    password generate

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,14 +13,10 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
+char *aux = dest;
+while (*src)
+*dest++ = *src++;
 
-int index = 0, src_len = 0;
-while (src[index++])
-src_len++;
-for (index = 0; src[index] && index < n; index++)
-dest[index] = src[index];
-for (index = src_len; index < n; index++)
-dest[index] = '\0';
+return (aux);
 
-return (index);
 }
