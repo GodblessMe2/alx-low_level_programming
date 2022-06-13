0408ec384c763a574d4ac459d6d1f7241be253ff
GodblessMe2 <oshindeinde@gmail.com>
2022-06-13 22:45:13 +0000

    changes

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,10 +6,11 @@
  *
  * Return: lenght
  */
+
 int _strlen(char *s)
 {
-	int len = 0;
-	while (s[len] != '\0')
-		len++;
-	return (len);
+int len = 0;
+while (s[len] != '\0')
+len++;
+return (len);
 }
