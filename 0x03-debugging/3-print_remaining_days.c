78edc1a6a41bcd3f0c1c69fee2a206c87db981e7
GodblessMe2 <oshindeinde@gmail.com>
2022-06-16 13:37:37 +0000

    changes

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -19,13 +19,13 @@ if (month >= 2)
 day++;
 }
 printf("Day of the year: %d\n", day);
-printf("Remaining days: %d\n", 366 - day);
+printf("Remaining days: %d\n", 367 - day);
 }
 else
 {
 if (month == 2 && day == 60)
 {
-printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
+printf("Invalid date: %02d/%02d/%04d\n", month, day - 32, year);
 }
 else
 {
@@ -33,5 +33,4 @@ printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 365 - day);
 }
 }
-}
-
+}i
