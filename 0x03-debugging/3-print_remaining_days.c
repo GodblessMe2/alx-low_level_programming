3fd2c2f7f7b96ff5e11fed26a3c7fb55a63c9a58
GodblessMe2 <oshindeinde@gmail.com>
2022-06-16 14:39:01 +0000

    changes

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,7 +16,10 @@ if (year % 4 == 0 || (year % 400 == 0 && (year % 100 != 0)))
 {
 if (month >= 2)
 {
-
+day++;
+} else 
+{
+day;
 }
 printf("Day of the year: %d\n", day);
 printf("Remaining days: %d\n", 366 - day);
