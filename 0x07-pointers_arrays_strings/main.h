77ecaf0f0e17bce65b0805985c07380828ce6b88
GodblessMe2 <oshindeinde@gmail.com>
2022-06-20 19:46:02 +0000

    changes on memory bytes

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,8 +1,3 @@
-#ifndef PUTCHAR_H
-#define PUTCHAR_H
-
-#include <stddef.h>
-
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -13,5 +8,3 @@ char *strstr(char *haystack, char *needle);
 void print_chesseboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-
-#endif
