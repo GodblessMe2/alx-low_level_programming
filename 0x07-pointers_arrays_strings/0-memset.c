b8f41ef460a0ae7c5e9954927a7b17b98d7a21f8
GodblessMe2 <oshindeinde@gmail.com>
2022-06-20 21:39:00 +0000

    changes on memory bytes

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,10 +10,10 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-unsigned int index;
-unsigned char *memory = s, value = b;
-for (index = 0; index < n; index++)
-memory[index] = value;
-
-return (memory);
+unsigned int i;
+for (i = 0; i < n; i++)
+{
+*(s + i) = b;
+}
+return (s);
 }
