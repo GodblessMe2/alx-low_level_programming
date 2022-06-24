22dfe0313439c9923b6b25f84ff2b82db199f746
GodblessMe2 <oshindeinde@gmail.com>
2022-06-24 09:40:42 -0700

    static libraries in c programming

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_memcpy - copies memory area
+ * @dest: destination of copy
+ * @src: memory area to copy
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int mem;
+for (mem = 0; mem < n; mem++)
+{
+*(dest + mem) = *(src + mem);
+}
+return (dest);
+}
