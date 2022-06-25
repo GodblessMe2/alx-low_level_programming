e61e0db2100566a273247aa3b07673bf92ac98b1
GodblessMe2 <oshindeinde@gmail.com>
2022-06-24 18:07:13 -0700

    correction

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,16 +7,8 @@
  * @argv: Array name
  * Return: 0
  */
-
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-int i;
-if (argc == 1)
 printf("%d\n", argc - 1);
-else
-{
-for (i = 0; *argv; i++, argv++)
-printf("%d\n", i - 1);
-}
 return (0);
 }
